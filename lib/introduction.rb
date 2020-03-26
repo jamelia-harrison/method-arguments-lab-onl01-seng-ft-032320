fbb7222b6ade32266e65a2e0d9f67404d63f367c
jamelia harrison <jameliaharrison@gmail.com>
2020-03-26 20:55:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,3 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
-intorduction("Josh")
-
-def introduction_with_language (name, language)
-  puts "Hi my name is #{name} and I am leaning to program in #{language}."
-end
-
-introduction_with_language ("Jamelia" , "Ruby")
